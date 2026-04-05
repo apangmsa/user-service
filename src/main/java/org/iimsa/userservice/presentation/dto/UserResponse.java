b3be1c16063424ebde095a710f3f8aeaf8b49f78
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-04-06 02:54:41 +0900

    feat: UserResponse 수정

--- src/main/java/org/iimsa/userservice/presentation/dto/UserResponse.java
+++ src/main/java/org/iimsa/userservice/presentation/dto/UserResponse.java
@@ -10,6 +10,10 @@ import lombok.NoArgsConstructor;
 import org.iimsa.userservice.domain.model.Role;
 import org.iimsa.userservice.domain.model.Status;
 
+/**
+ * Info : 모든 정보 표시 (null 포함) 추후 조회 용도에 따라 DTO 분기 CompanyDeliveryInfo HubDeliveryInfo InfoToApprove
+ *
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserResponse {
     @AllArgsConstructor
@@ -27,23 +31,23 @@ public class UserResponse {
         private UUID id;
         @Schema(description = "사용자 이름")
         private String name;
-        @Schema(description = "사용자 역할")
-        private Role role;
         @Schema(description = "이메일")
         private String email;
         @Schema(description = "슬랙 ID")
         private String slackId;
-        @Schema(description = "소속 허브 ID")
-        private UUID hubId;
-        @Schema(description = "소속 허브명")
-        private String hubName;
-        @Schema(description = "배송 순번")
-        private Integer deliveryRotationOrder;
-        @Schema(description = "소속 업체 ID")
-        private UUID companyId;
-        @Schema(description = "소속 업체명")
-        private String companyName;
+        @Schema(description = "사용자 희망 역할")
+        private Role requestedRole;
+        @Schema(description = "사용자 역할 (MASTER, HUB_MANAGER, HUB_DELIVERY_MANAGER, COMPANY_MANAGER, COMPANY_DELIVERY_MANAGER)")
+        private Role role;
+        @Schema(description = "소속명")
+        private String associateName;
         @Schema(description = "사용자 승인 상태 (PENDING, APPROVED, REJECTED)")
         private Status status;
+        @Schema(description = "소속 허브 ID")
+        private UUID hubId;
+        @Schema(description = "소속 업체 ID")
+        private UUID companyId;
+        @Schema(description = "배송 순번")
+        private Integer deliverySequence;
     }
 }
