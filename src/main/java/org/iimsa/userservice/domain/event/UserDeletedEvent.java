91decc6eb55f3928f4246e348cdd98ad355ef0e6
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-04-06 11:30:03 +0900

    docs: UserDeletedEvent 계층 구조 관련 주석 추가

--- src/main/java/org/iimsa/userservice/domain/event/UserDeletedEvent.java
+++ src/main/java/org/iimsa/userservice/domain/event/UserDeletedEvent.java
@@ -2,6 +2,9 @@ package org.iimsa.userservice.domain.event;
 
 import org.iimsa.userservice.domain.model.User;
 
+/**
+ * 도메인 이벤트 발행 인터페이스 Application Service는 이 인터페이스만 알고, 실제 Kafka 구현은 Infrastructure에서 담당합니다.
+ */
 public interface UserDeletedEvent {
     void deleted(User user);
 }
